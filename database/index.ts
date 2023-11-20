cd13173a73d8ec0a1666cf64340c95ae5c99feed
jorge <1jorgevalles@gmail.com>
2023-11-20 17:12:31 -0500

    first commit

---
+++ database/index.ts
@@ -0,0 +1,12 @@
+
+
+export * as db from './db';
+export * as dbProducts from './dbProducts';
+export * from './constants';
+
+
+export * as dbUsers from './dbUsers';
+export * as dbOrders from './dbOrders';
+// Base de datos de prueba
+export * as seedDatabase from './seed-data';
+
---
+++ interfaces/index.ts
@@ -0,0 +1,8 @@
+
+
+export * from './cart';
+export * from './dashboard';
+export * from './products';
+export * from './user';
+export * from './order';
+export * as IPaypal from './paypal';
